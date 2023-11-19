c7aa76a7c5364b7172b44b621a69b204b2170c9c
Tadas Laurinaitis <tadas.laurinaitis100@gmail.com>
2023-11-19 23:48:12 +0200

    ApiKeyProvider implementation
    Implemented a class which is responsible for getting the API Key from file

---
+++ Shared/Providers/ApiKeyProvider.cs
@@ -0,0 +1,35 @@
+﻿namespace Shared.Providers
+{
+    public sealed class ApiKeyProvider
+    {
+        private ApiKeyProvider() { }
+
+        static ApiKeyProvider() { }
+
+        private static ApiKeyProvider instance = new ApiKeyProvider();
+
+        public static ApiKeyProvider Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        private const string ApiKeyLocationFolder = @"D:\Projects\Locations\ApiKeyLocation.txt";
+
+        private string apiAccessKey = "";
+
+        public string GetApiAccessKey()
+        {
+            if(!string.IsNullOrEmpty(apiAccessKey))
+            {
+                return apiAccessKey;
+            }
+
+            apiAccessKey = File.ReadAllText(File.ReadAllText(ApiKeyLocationFolder));
+
+            return apiAccessKey;
+        }
+    }
+}
